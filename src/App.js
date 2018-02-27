f83ad93563458f3373bd5979b30640f0324fbec0
sdede <sandro.dede@clevermind.fr>
2018-02-27 12:43:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+//import LoginForm                  from './components/LoginForm';
+import './App.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.prenom} {props.nom}</h1>;
+}
+
+function Test (props) {
+  return <h2> Test {props.toto}</h2>;
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Welcome prenom="Sara" nom="Connor" />
+      <Test toto="toto" />
+      <Welcome prenom="Cahal" nom="Connor" />
+      <Welcome prenom="Edith" nom="Connor" />
+     </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+}
+
+export default withRouter(LoginForm);
